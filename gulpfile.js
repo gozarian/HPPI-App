a4e4f457167d5eb5f9e075280d91cc96a2999fcd
James Jacoby <james@builtbymoby.com>
2016-12-04 22:50:44 -0800

    adds test controller, js build and separate templates js file

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,23 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var jscs = require('gulp-jscs');
+var ngTemplates = require('gulp-angular-templatecache');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  styles: ['./src/styles/**/*.scss'],
+  scripts: ['./src/scripts/**/*.js', './src/templates/**/*.html']
 };
 
-gulp.task('default', ['sass']);
+var scripts = [
+  './src/scripts/app.js',
+  './src/scripts/controllers/test-controller.js',
+];
 
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
+gulp.task('default', ['styles', 'scripts']);
+
+gulp.task('styles', function(done) {
+  gulp.src('./src/styles/ionic.app.scss')
     .pipe(sass())
     .on('error', sass.logError)
     .pipe(gulp.dest('./www/css/'))
@@ -26,26 +34,19 @@ gulp.task('sass', function(done) {
     .on('end', done);
 });
 
-gulp.task('watch', ['sass'], function() {
-  gulp.watch(paths.sass, ['sass']);
+gulp.task('scripts', ['templates'], function() {
+  gulp.src(scripts)
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./www/js/'));
 });
 
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
+gulp.task('templates', function() {
+  return gulp.src('./src/templates/**/*.html')
+    .pipe(ngTemplates('templates.js', { standalone: true }))
+    .pipe(gulp.dest('www/js/'));
 });
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
+gulp.task('watch', ['default'], function() {
+  gulp.watch(paths.styles, ['styles']);
+  gulp.watch(paths.scripts, ['scripts']);
 });
