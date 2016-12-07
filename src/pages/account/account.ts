5d0251b89a317ab2608bc02f79e3bc0277a8bbe7
pstrum <pstrum@gmail.com>
2016-12-07 13:15:34 -0800

    Add structure to Account Page template

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { PersonalInfoPage } from '../personal-info/personal-info';
+import { BillingPage } from '../billing/billing';
+import { PaymentPage } from '../payment/payment';
+import { PasswordPage } from '../password/password';
+import { PolicyPage } from '../policy/policy';
+import { FAQPage } from '../faq/faq';
+import { ContactPage } from '../contact/contact';
 
 @Component({
   selector: 'page-account',
@@ -8,8 +14,52 @@ import { NavController } from 'ionic-angular';
 })
 export class AccountPage {
 
+  information = [
+    { title: 'Personal Info',
+      component: PersonalInfoPage,
+      notification: false
+    },
+    { title: 'Policy and Billing Info',
+      component: BillingPage,
+      notification: true
+    },
+    { title: 'Edit Payment Method',
+      component: PaymentPage,
+      notification: false
+    },
+    { title: 'Change Password',
+      component: PasswordPage,
+      notification: false
+    }
+  ];
+
+  policies = [
+    { title: 'Jackson\'s Policy',
+      component: PolicyPage,
+      notification: false
+    },
+    { title: 'Vincent\'s Policy',
+      component: PolicyPage,
+      notification: false
+    }
+  ];
+
+  support = [
+    { title: 'Help & FAQ\s',
+      component: FAQPage,
+      notification: false
+    },
+    { title: 'Contact Healthy Paws',
+      component: ContactPage,
+      notification: false
+    }
+  ];
+
   constructor(public navCtrl: NavController) {
 
   }
 
+  itemSelected(item) {
+    this.navCtrl.setRoot(item.component);
+  }
 }
