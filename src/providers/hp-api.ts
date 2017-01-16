eee8d6f4a9cbac34f1c4b797cef3b210ac1bc101
James Jacoby <james@builtbymoby.com>
2017-01-16 13:54:26 -0800

    hush linter error

--- src/providers/hp-api.ts
+++ src/providers/hp-api.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/share';
 
-import { Account } from '../models/account';
+// import { Account } from '../models/account';
 import { Environment } from '../providers/environment';
 
 @Injectable()
