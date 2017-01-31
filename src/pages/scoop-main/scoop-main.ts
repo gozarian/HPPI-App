f8dcb594c6020db81016db00a12877844b0537a9
pstrum <pstrum@gmail.com>
2017-01-31 10:50:37 -0800

    WIP: Add browser lite buttons and access to iframe `contentWindow`
    * Note: browser buttons are `display: none`, since functionality currently
    is not implemented.
    * This commit accesses the iframe for "The Scoop" in it's controller,
    but accessing its history throws up CORS errors that need to be
    worked around.

--- src/pages/scoop-main/scoop-main.ts
+++ src/pages/scoop-main/scoop-main.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { NavController, LoadingController } from 'ionic-angular';
 import { Environment } from '../../providers/environment';
@@ -9,7 +9,9 @@ import { Environment } from '../../providers/environment';
 })
 export class ScoopMainPage implements OnInit {
 
+  @ViewChild('scoopFrame') iframe: ElementRef;
   loading;
+  frameWindow;
   scoopUrl: SafeResourceUrl;
 
   constructor(
@@ -22,6 +24,8 @@ export class ScoopMainPage implements OnInit {
 
   ngOnInit() {
     this.presentLoading();
+    this.frameWindow =  this.iframe.nativeElement.contentWindow;
+    console.log(this.frameWindow);
   }
 
   presentLoading() {
@@ -30,5 +34,15 @@ export class ScoopMainPage implements OnInit {
       duration: 2000
     });
     this.loading.present();
+  }
+
+  navBack() {
+    // TODO: manuipulate iframe window history object
+    // this.frameWindow.history.back()
+  }
+
+  navForward() {
+    // TODO: manuipulate iframe window history object
+    // this.frameWindow.history.go(1)
   }
 }
